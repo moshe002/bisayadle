036f986a34ffb1951ceffac885480795f2f3eb23
moshe002 <mosesfat@gmail.com>
2026-09-05 19:49:48 +0800

    feat: add handling of backspace and enter & qol updates

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -12,6 +12,12 @@ interface HowToPlaceProps {
 const HowToPlay: React.FC<HowToPlaceProps> = ({ 
     setHowToPlay 
 }) => {
+
+  const handleClose = () => {
+    localStorage.setItem('hasSeenHowToPlay', 'true');
+    setHowToPlay(false);
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-400 bg-opacity-20 backdrop-filter backdrop-blur-sm">
       <div className="flex flex-col border-2 border-gray-300 h-3/4 items-center rounded-lg p-7 opacity-100 text-center bg-white shadow-2xl overflow-y-auto">
@@ -28,7 +34,7 @@ const HowToPlay: React.FC<HowToPlaceProps> = ({
           <button 
             className='p-3 bg-green-400 text-white rounded-md font-semibold opacity-70 hover:opacity-100 duration-150' 
             type="button" 
-            onClick={() => setHowToPlay(false)}
+            onClick={handleClose}
             >
                 SIRADO
             </button>
